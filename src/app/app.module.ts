51b69995ce559471bdd8a6feea2886050bebc396
renatachagasc <rehcardosochagas@gmail.com>
2019-05-31 14:44:18 -0300

    primeiro commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AdicionarComponent } from './Usuarios/adicionar/adicionar.component';
+import { EditarComponent } from './Usuarios/editar/editar.component';
+import { ListarComponent } from './Usuarios/listar/listar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdicionarComponent,
+    EditarComponent,
+    ListarComponent
   ],
   imports: [
     BrowserModule,
